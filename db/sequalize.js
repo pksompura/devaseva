c524ad4c4a631cb63fed609802bdad7321d60de0
chandu88611 <aschandfan88@gmail.com>
2024-08-30 07:42:16 +0530

    deva seva commit

--- db/sequalize.js
+++ db/sequalize.js
@@ -1,46 +1,46 @@
-// // db/sequelize.js
-// import { Sequelize } from 'sequelize';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const databaseUrl = process.env.DATABASE_URL || 'postgresql://temple_postgress_user:IPD10NINu3ZgDFOSjvmedLt3HLD0xRVe@dpg-cqhnilqju9rs738obeu0-a.singapore-postgres.render.com/temple_postgress';
-
-// let sequelize;
-
-// if (!sequelize) {
-//   console.log('Connecting to database:', databaseUrl); // Debug log for verifying the URL
-
-//   sequelize = new Sequelize(databaseUrl, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false, // Render's PostgreSQL usually requires SSL
-//       }
-//     },
-//     logging: console.log,  
-//   });
-// }
-
-// export default sequelize;
-
 // db/sequelize.js
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST  ,
-  database: process.env.DB_DATABASE ,
-  username: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD  ,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: false, // Disable SSL
-  },
-  logging: console.log,  
-});
+const databaseUrl = 'postgresql://devaseva_user:Oar58ox0gCgBGx0G4ZdxXPXfJrjqR5l3@dpg-cr8ijgq3esus73b7987g-a.oregon-postgres.render.com/devaseva';
+
+let sequelize;
+
+if (!sequelize) {
+  console.log('Connecting to database:', databaseUrl); // Debug log for verifying the URL
+
+  sequelize = new Sequelize(databaseUrl, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Render's PostgreSQL usually requires SSL
+      }
+    },
+    logging: console.log,  
+  });
+}
 
 export default sequelize;
+
+// db/sequelize.js
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const sequelize = new Sequelize({
+//   host: process.env.DB_HOST  ,
+//   database: process.env.DB_DATABASE ,
+//   username: process.env.DB_USER ,
+//   password: process.env.DB_PASSWORD  ,
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: false, // Disable SSL
+//   },
+//   logging: console.log,  
+// });
+
+// export default sequelize;
