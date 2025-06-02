9c6ece71f56eb3769b87d20685abd79e4cb59217
diva-sl <divakaran.sivasamy@gmail.com>
2025-06-02 17:44:20 +0530

    jcnjkkjd

--- server.js
+++ server.js
@@ -63,7 +63,14 @@ const __dirname = path.dirname(__filename);
 
 // app.use(cors(corsOptions));
 // app.options("*", cors(corsOptions)); // Preflight
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*", // allows all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // app.use(cors(corsOptions));
 // app.use(cors("*"));
