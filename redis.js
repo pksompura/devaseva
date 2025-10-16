460d58fb0ae04c234ad53af08076d38439912298
diva-sl <divakaran.sivasamy@gmail.com>
2025-10-16 14:38:24 +0530

    update the file

--- redis.js
+++ redis.js
@@ -7,11 +7,16 @@
 //   maxRetriesPerRequest: null,
 // });
 // redis.js
+// import { Redis } from "ioredis";
+
+// export const redisConnection = new Redis(
+//   process.env.REDIS_URL || "redis://127.0.0.1:6379",
+//   {
+//     maxRetriesPerRequest: null,
+//   }
+// );
 import { Redis } from "ioredis";
 
-export const redisConnection = new Redis(
-  process.env.REDIS_URL || "redis://127.0.0.1:6379",
-  {
-    maxRetriesPerRequest: null,
-  }
-);
+export const redisConnection = new Redis("redis://127.0.0.1:6379", {
+  maxRetriesPerRequest: null,
+});
