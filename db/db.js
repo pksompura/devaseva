1888ddff75d2ada7dcfdb4c99670e36de853944f
diva-sl <divakaran.sivasamy@gmail.com>
2026-03-04 10:37:41 +0530

    updated mongourl

--- db/db.js
+++ db/db.js
@@ -1,19 +1,3 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(
-//       "mongodb+srv://pksompura_db_user:pksompura123@cluster0.oglmcl4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-//     );
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
