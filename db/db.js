9dc5ce1fc2911e9e65296d10c2ba9b58cd1f7d71
diva-sl <divakaran.sivasamy@gmail.com>
2026-03-04 10:32:48 +0530

    updated redis and mongourl now get data from mongo properly tested

--- db/db.js
+++ db/db.js
@@ -1,13 +1,30 @@
-import mongoose from 'mongoose';
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(
+//       "mongodb+srv://pksompura_db_user:pksompura123@cluster0.oglmcl4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+//     );
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://aschandan88:Chandu8861151876@cluster0.vmxomxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`✅ MongoDB Connected (${conn.connection.name})`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`❌ DB Error: ${error.message}`);
     process.exit(1);
   }
-}; 
+};
 
 export default connectDB;
